7a3084520154847989bdef326c1f819d8de019e3
Luisa Perea <lfernandaperea@uniajc.edu.co>
2024-10-25 21:54:17 -0500

    ProyectoFinal1

---
+++ e-commerce.js
@@ -0,0 +1,52 @@
+const productos = [
+    // Sección Belleza
+    { nombre: 'Agua de Rosas', categoria: 'belleza', imagen: './img/belleza/AguaRosa.jpg' },
+    { nombre: 'Base Líquida', categoria: 'belleza', imagen: './img/belleza/baseLiquida.jpg' },
+    { nombre: 'Brillo Maybelline', categoria: 'belleza', imagen: './img/belleza/brilloMaybelline.jpg' },
+    { nombre: 'Crema Facial Día', categoria: 'belleza', imagen: './img/belleza/cremafacialDia.jpg' },
+    { nombre: 'Crema Facial Noche', categoria: 'belleza', imagen: './img/belleza/cremaFacialnoche.jpg' },
+    { nombre: 'Labial Maybelline', categoria: 'belleza', imagen: './img/belleza/labialMaybelline.jpg' },
+    { nombre: 'Limpiador Facial', categoria: 'belleza', imagen: './img/belleza/limpiadorFacial.jpg' },
+    { nombre: 'Pestañina Loreal', categoria: 'belleza', imagen: './img/belleza/pestañinaLoreal.jpg' },
+    { nombre: 'Serum Antimanchas Garnier', categoria: 'belleza', imagen: './img/belleza/serumAntimanchasGarnier.jpg' },
+    { nombre: 'Suero para Pestañas', categoria: 'belleza', imagen: './img/belleza/SueroPestañas.jpg' },
+
+    // Sección Cuidado Personal
+    { nombre: 'Bloqueador Solar', categoria: 'cuidado personal', imagen: './img/cuidado personal/BloqueadorSolar.jpg' },
+    { nombre: 'Cepillo dental', categoria: 'cuidado personal', imagen: './img/cuidado Personal/cepilloDental.jpg' },
+    { nombre: 'Crema Dental', categoria: 'cuidado personal', imagen: './img/cuidado Personal/cremaDental.png' },
+    { nombre: 'Crema Facial', categoria: 'cuidado personal', imagen: './img/cuidado Personal/cremaFacial.jpg' },
+    { nombre: 'Enjuague Bucal', categoria: 'cuidado personal', imagen: './img/cuidado Personal/EnjuagueBucal.jpg' },
+    { nombre: 'Hidratante', categoria: 'cuidado personal', imagen: './img/cuidado Personal/hidratanteNeutrogena.jpg' },
+    { nombre: 'Protector Labial', categoria: 'cuidado personal', imagen: './img/cuidado Personal/protectorLabial.jpg' },
+    { nombre: 'Seda Dental', categoria: 'cuidado personal', imagen: './img/cuidado Personal/SedaDental.jpg' },
+    { nombre: 'Shampoo Dove', categoria: 'cuidado personal', imagen: './img/cuidado Personal/Shampoodove.jpg' },
+    { nombre: 'Tratamiento Capilar', categoria: 'cuidado personal', imagen: './img/cuidado Personal/tratamientoCapilar.jpg' },
+
+
+    // Sección de Medicamentos
+    { nombre: 'Algodon', categoria: 'medicamentos', imagen: './img/medicamentos/Algodon.jpg' },
+    { nombre: 'Cura Cure', categoria: 'medicamentos', imagen: './img/medicamentos/CuraCure.png' },
+    { nombre: 'Dolex Gripa', categoria: 'medicamentos', imagen: './img/medicamentos/DolexGripa.jpg' },
+    { nombre: 'Electrolit', categoria: 'medicamentos', imagen: './img/medicamentos/Electrolit.jpg' },
+    { nombre: 'Mieltertos', categoria: 'medicamentos', imagen: './img/medicamentos/Mieltertos.jpg' },
+    { nombre: 'Noxpirin', categoria: 'medicamentos', imagen: './img/medicamentos/Noxpirin.jpg' },
+    { nombre: 'Pax Dia', categoria: 'medicamentos', imagen: './img/medicamentos/PacDia.jpg' },
+    { nombre: 'PaxNoche', categoria: 'medicamentos', imagen: './img/medicamentos/PaxNoche.jpg' },
+    { nombre: 'Vaporub', categoria: 'medicamentos', imagen: './img/medicamentos/Vaporub.jpg' },
+    { nombre: 'VitaminaC', categoria: 'medicamentos', imagen: './img/medicamentos/VitaminaC.jpg' },
+
+
+
+    // Sección Cuidado del Bebé
+    { nombre: 'Aceite Bebe', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/aceiteBaby.jpg' },
+    { nombre: 'Cereal Infantil', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/cerealInfantil.jpg' },
+    { nombre: 'Compota', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/compota.jpg' },
+    { nombre: 'Crema Antipañalitis', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/cremaAntipañalitis.jpg' },
+    { nombre: 'Crema para peinar', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/cremaPeinar.png' },
+    { nombre: 'Formula Infantil similac', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/FormulaInfantil.jpg' },
+    { nombre: 'Jabon liquido', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/jabonLiquido.png' },
+    { nombre: 'Shampoo Baby', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/shampooBaby.jpg' },
+    { nombre: 'Vaselina Baby', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/vaselinaBaby.jpg' },
+    { nombre: 'yogurthBaby', categoria: 'cuidado del bebe', imagen: './img/cuidado del bebe/yogurthBaby.png' },
+];
